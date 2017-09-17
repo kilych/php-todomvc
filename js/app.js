93b8d8117e4e76ea89a4f2820edb9b79525bd696
Polina <paluhanv@gmail.com>
2017-09-17 15:33:30 +0700

    Complete ugly js

--- js/app.js
+++ js/app.js
@@ -1,53 +1,100 @@
 (function (window) {
-	'use strict';
+  'use strict';
 
-	// Your starting point. Enjoy the ride!
+  // Your starting point. Enjoy the ride!
 
-    $(document).ready(function () {
-      setTodoCount();
+  $(document).ready(function () {
+    var ENTER_KEY = 13;
 
-      $('.toggle').click(toggle);
-
-      $("label[for='toggle-all']").click(toggleAll);
-
-      $('button.destroy').click(remove);
-
-      $('button.clear-completed').click(removeCompleted);
+    $('.new-todo').keypress(function (event) {
+      if (event.which === ENTER_KEY) {
+        add($(this));
+      }
     });
 
-    function add() {}
+    $("label[for='toggle-all']").click(toggleAll);
 
-    function remove() {
-      var button = $(this);
+    $('button.clear-completed').click(removeCompleted);
 
-      button.parent().parent().remove();
-      setTodoCount();
+    init();
+});
+
+  function init() {
+    $('button.destroy').click(remove);
+
+    $('.toggle').click(toggle);
+
+    setTodoCount();
+  }
+
+  function add(target) {
+    var text = target.val().trim();
+    target.val('');
+
+    if (text) {
+      text = encode(text);
+
+      $('ul.todo-list').append(
+        '<li><div class="view"><input class="toggle" type="checkbox"><label>'
+          + text + '</label><button class="destroy"></button></div><input class="edit" value=""></li>'
+      );
+
+      init();
+    }
+  }
+
+  function encode(str) {
+    var p = document.createElement("p");
+    p.textContent = str;
+    var converted = p.innerHTML;
+
+    return converted;
+  }
+
+  function remove() {
+    var button = $(this);
+
+    button.parent().parent().remove();
+    setTodoCount();
+  }
+
+  function removeCompleted() {
+    var completed = $('ul.todo-list > li.completed');
+
+    completed.remove();
+    setTodoCount();
+  }
+
+  function count() {
+    var todos = $('ul.todo-list > li');
+    var completed = $('ul.todo-list > li.completed');
+
+    return todos.length - completed.length;
+  }
+
+  function setTodoCount() {
+    var target = $('span.todo-count');
+    var counter = count();
+    var str = 'items';
+    if (counter === 1) str = 'item';
+
+    target.html('<strong>' + counter + '</strong> ' + str + ' left');
+  }
+
+  function toggle() {
+    var target = $(this);
+
+    if (target.is(':checked')) {
+      target.parent().parent().addClass('completed');
+    } else {
+      target.parent().parent().removeClass('completed');
     }
 
-    function removeCompleted() {
-      var completed = $('ul.todo-list > li.completed');
+    setTodoCount();
+  }
 
-      completed.remove();
-      setTodoCount();
-    }
-
-    function count() {
-      var todos = $('ul.todo-list > li');
-      var completed = $('ul.todo-list > li.completed');
-
-      return todos.length - completed.length;
-    }
-
-    function setTodoCount() {
-      var target = $('span.todo-count');
-      var counter = count();
-      var str = 'items';
-      if (counter === 1) str = 'item';
-
-      target.html('<strong>' + counter + '</strong> ' + str + ' left');
-    }
-
-    function toggle() {
+  function toggleAll() {
+    $('.toggle').each(function () {
       var target = $(this);
 
       if (target.is(':checked')) {
@@ -57,29 +104,16 @@
       }
 
       setTodoCount();
+    });
+  }
+
+  function toggleCheckbox(target) {
+    if (target.is(':checked')) {
+      target.parent().parent().addClass('completed');
+    } else {
+      target.parent().parent().removeClass('completed');
     }
+  }
 
-    function toggleAll() {
-      $('.toggle').each(function () {
-        var target = $(this);
-
-        if (target.is(':checked')) {
-          target.parent().parent().addClass('completed');
-        } else {
-          target.parent().parent().removeClass('completed');
-        }
-
-        setTodoCount();
-      });
-    }
-
-    function toggleCheckbox(target) {
-      if (target.is(':checked')) {
-        target.parent().parent().addClass('completed');
-      } else {
-        target.parent().parent().removeClass('completed');
-      }
-    }
-
-    function edit() {}
+  function edit() {}
 })(window);
